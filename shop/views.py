b680fb2b0fc1c72986801c0dbab30dc54e32b0e3
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-28 00:50:47 +0530

    showed multiple products on homepage

--- shop/views.py
+++ shop/views.py
@@ -10,7 +10,10 @@ def index(request):
     print(products)
     n = len(products)
     nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range':range(1,nSlides),'product':products}
+    # params = {'no_of_slides':nSlides, 'range':range(1,nSlides),'product':products}
+    allProds = [[products, range(1,nSlides), nSlides],
+               [products, range(1,nSlides), nSlides]]
+    params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
 def about(request):
