eaf82fdd68626cf53ac2dcee134d492e2655294e
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-27 16:34:39 +0530

    Project Layout Initiated and Woking

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='shop-index'),
+    path('', views.index, name='shopHome'),
+    path('about/', views.about, name='AboutUs'),
+    path('contact/', views.contact, name='ContactUs'),
+    path('tracker/', views.tracker, name='TrackingStatus'),
+    path('search/', views.search, name='Search'),
+    path('productView', views.productView, name='ProductView'),
+    path('checkout', views.checkout, name='Checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def search(request):
+    return render(request, 'shop/search.html')
+
+def productView(request):
+    return render(request, 'shop/productView.html')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
