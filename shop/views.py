dcafe56c84bca7a9268aab19530f89d937c50ed2
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-28 01:07:31 +0530

    Category Wise Product Display Done

--- shop/views.py
+++ shop/views.py
@@ -6,13 +6,21 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+
     # params = {'no_of_slides':nSlides, 'range':range(1,nSlides),'product':products}
-    allProds = [[products, range(1,nSlides), nSlides],
-               [products, range(1,nSlides), nSlides]]
+    # allProds = [[products, range(1,nSlides), nSlides],
+    #            [products, range(1,nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1,nSlides), nSlides])
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
